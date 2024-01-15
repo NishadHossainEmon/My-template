94790cd481954b3caa1541a1710b9157d68cd946
NishadHossainEmon <141209145+NishadHossainEmon@users.noreply.github.com>
2024-01-15 23:41:26 +0600

    Add files via upload

---
+++ 349.cpp
@@ -0,0 +1,22 @@
+/*********************** In the Name of ALLAH ************************/
+#include<bits/stdc++.h>
+#define fl(n) for(ll i = 0; i < n;i++)
+#define flj(n) for(ll j = 0; j < n;j++)
+#define optimize() ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+#define ll long long
+#define endl "\n"
+using namespace std;
+ll  mod = 10000009;
+
+int main()
+{
+    optimize();
+    ll t;
+    cin >> t;
+    while(t--)
+    {
+        ll n,m,a,b,c,d,x,y,z,count = 0,count2 = 0,sum = 0,sum2 = 0,ans = 0;
+        string s,str;
+    }
+}
+
